857951fe77f7a818b5956cec223d7f58dd255953
Brian Chen <bchen.chen93@gmail.com>
2015-07-19 20:47:26 +1200

    Added model. separated concerns for data. Added first marker for restaurants

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 var viewModel = function() {
-
+  var init = function() {
+    var mapOptions = {
+      center: model.initialMap.center,
+      zoom: model.initialMap.zoom
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  }
 };
 
 ko.applyBindings(viewModel);
 
 var initializeMap = function() {
       var mapOptions = {
-        center: { lat: -36.897775, lng: 174.906409},
-        zoom: 15
+        center: model.initialMap.center,
+        zoom: model.initialMap.zoom
       };
       var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: model.markers[0].position,
+        title: model.markers[0].title
+      });
+
+      marker.setMap(map);
     }
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
---
+++ js/model.js
@@ -0,0 +1,13 @@
+var model = {
+  "initialMap":
+  {
+    "center": new google.maps.LatLng(-36.897745,174.906407),
+    "zoom": 15
+  },
+  "markers": [
+    {
+      "title": "Tanpopo Ramen",
+      "position": new google.maps.LatLng(-36.899596,174.904265)
+    }
+  ]
+}
