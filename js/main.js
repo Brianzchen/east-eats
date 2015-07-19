209498459aac7c2a4030b63a59a3f23551a67d97
Brian Chen <bchen.chen93@gmail.com>
2015-07-19 16:34:08 +1200

    Added map of desired location

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var viewModel = function() {
 };
 
 ko.applyBindings(viewModel);
+
+var initializeMap = function() {
+      var mapOptions = {
+        center: { lat: -36.897775, lng: 174.906409},
+        zoom: 15
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    }
+
+google.maps.event.addDomListener(window, 'load', initializeMap);
