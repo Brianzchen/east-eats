67b8f01c712a16068d2de7538bcd8186dd9df68b
Brian Chen <bchen.chen93@gmail.com>
2016-10-10 20:27:54 +1300

    feat: Maps showing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div id="map"></div>,
   document.getElementById('container')
 );
+
+var map;
+window.initMap = function() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
