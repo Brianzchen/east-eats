790eded0b6bea762e28f9cac0cf4ae9807288c01
Brian Chen <bchen.chen93@gmail.com>
2016-10-10 22:06:14 +1300

    fix: Map always loads

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-ReactDOM.render(
-  <div id="map"></div>,
-  document.getElementById('container')
-);
-
 var map;
 window.initMap = function() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: -36.914086, lng: 174.905668},
+    zoom: 14
   });
 }
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div id="main">
+        <h1 id="title">East Eats</h1>
+        <div id="map"></div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById("container")
+);
