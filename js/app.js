ae19d403effb6556f431ad23c174c4341522bbd1
Brian Chen <bchen.chen93@gmail.com>
2016-10-14 20:09:23 +1300

    feat: Hamburger icon looking the good

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ class Main extends React.Component {
         <div id="title" className="col-xl-1">
           <h1 className="text-center">East Eats</h1>
         </div>
-        <div id="toolBar">
+        <div id="toolBar" className="col-xl-1">
           <div id="searchBox" className="form-group">
             <input type="text" className="form-control" id="searchInput" placeholder="Search" />
           </div>
-          <div id="options"></div>
+          <div id="menuIcon"></div>
         </div>
         <div id="map"></div>
       </div>
