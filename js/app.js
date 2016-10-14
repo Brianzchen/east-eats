a42949fd39ca37ed3e0f6f02a5aa8d4077539f80
Brian Chen <bchen.chen93@gmail.com>
2016-10-14 19:33:31 +1300

    feat: Search bar and title displayed correctly

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-var map;
-window.initMap = function() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -36.914086, lng: 174.905668},
-    zoom: 14
-  });
-}
-
 class Main extends React.Component {
   render() {
     return (
-      <div id="main">
-        <h1 id="title">East Eats</h1>
+      <div id="main" className="container-fluid">
+        <div id="title" className="col-xl-1">
+          <h1 className="text-center">East Eats</h1>
+        </div>
+        <div id="toolBar">
+          <div id="searchBox" className="form-group">
+            <input type="text" className="form-control" id="searchInput" placeholder="Search" />
+          </div>
+          <div id="options"></div>
+        </div>
         <div id="map"></div>
       </div>
     );
@@ -21,3 +21,11 @@ ReactDOM.render(
   <Main />,
   document.getElementById("container")
 );
+
+var map;
+window.initMap = function() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -36.914086, lng: 174.905668},
+    zoom: 14
+  });
+}
